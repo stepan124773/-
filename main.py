6840c3d74b442741e0e63711534e0aab2d760e96
777 <chubakovstepan94@gmail.com>
2021-01-31 18:01:07 +0300

    2

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import sqlite3
 con = sqlite3.connect('coffee.sqlite')
 
 cur = con.cursor()
-
+# https://github.com/stepan124773/-.git 8e985f5
 cursor = con.execute('select * from coffee')
 names = [e[0] for e in cursor.description]
 
@@ -19,7 +19,11 @@ class MyWidget(QMainWindow):
         self.table = QTableWidget(self)
 
         self.gridLayout_2.addWidget(self.table, 0, 0)
-        # self.pushButton_2.clicked.connect(self.dobav)
+        self.pushButton.clicked.connect(self.dobav)
+        self.tablee()
+
+    def dobav(self):
+        ex2.show()
         self.tablee()
 
     def tablee(self):
@@ -43,6 +47,64 @@ class MyWidget(QMainWindow):
             self.table.setHorizontalHeaderItem(i, item)
 
 
+class MyWidget2(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('addEditCoffeeForm.ui', self)
+        self.pushButton.clicked.connect(self.delete)
+        self.pushButton_2.clicked.connect(self.dobav)
+        self.pushButton_3.clicked.connect(self.uzmen)
+        self.ids = [el[0] for el in cur.execute('''SELECT ID from coffee ''').fetchall()]
+
+    def uzmen(self):
+        self.id = self.lineEdit.text()
+        self.name = self.lineEdit_2.text()
+        self.degree = self.lineEdit_3.text()
+        self.ground = self.lineEdit_4.text()
+        self.taste = self.lineEdit_5.text()
+        self.price = self.lineEdit_6.text()
+        self.volume = self.lineEdit_7.text()
+        cur.execute('''UPDATE coffee
+                SET name = ? 
+                    WHERE ID = ?''', (self.name, self.id,))
+        cur.execute('''UPDATE coffee
+                SET degree = ? 
+                    WHERE ID = ?''', (self.degree, self.id,))
+        cur.execute('''UPDATE coffee
+                SET ground = ? 
+                    WHERE ID = ?''', (self.ground, self.id,))
+        cur.execute('''UPDATE coffee
+                SET taste = ? 
+                    WHERE ID = ?''', (self.taste, self.id,))
+        cur.execute('''UPDATE coffee
+                SET price = ? 
+                    WHERE ID = ?''', (self.price, self.id,))
+        cur.execute('''UPDATE coffee
+                SET volume = ? 
+                    WHERE ID = ?''', (self.volume, self.id,))
+        con.commit()
+
+    def dobav(self):
+        self.id = 1
+        while self.id in self.ids:
+            self.id += 1
+        self.name = self.lineEdit_2.text()
+        self.degree = self.lineEdit_3.text()
+        self.ground = self.lineEdit_4.text()
+        self.taste = self.lineEdit_5.text()
+        self.price = self.lineEdit_6.text()
+        self.volume = self.lineEdit_7.text()
+        cur.execute('''INSERT INTO coffee(ID,name,degree,ground, taste, price,volume) VALUES(?,?,?,?,?,?,?)''',
+                    (self.id, self.name, self.degree, self.ground, self.taste, self.price, self.volume))
+
+    con.commit()
+
+    def delete(self):
+        cur.execute('''DELETE from coffee
+        where ID = ?''', (int(self.lineEdit.text()),)).fetchall()
+        con.commit()
+
+
 def except_hook(cls, exception, traceback):
     sys.__excepthook__(cls, exception, traceback)
 
@@ -51,7 +113,7 @@ if __name__ == '__main__':
     sys.excepthook = except_hook
     app = QApplication(sys.argv)
     ex = MyWidget()
-
+    ex2 = MyWidget2()
     ex.show()
 
     sys.exit(app.exec())
